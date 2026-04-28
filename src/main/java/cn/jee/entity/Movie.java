f0fcbecd8487617edf137ee1230696ef20269182
userName <userEmail>
2026-04-28 11:30:53 +0800

    完善上传图片页面

--- src/main/java/cn/jee/entity/Movie.java
+++ src/main/java/cn/jee/entity/Movie.java
@@ -19,6 +19,7 @@ import java.util.List;
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Movie {
 
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @EqualsAndHashCode.Include
